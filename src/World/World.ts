8d70e86cb2b67a7c0c057af5769ff671e9672d62
steamdeck <wuchen.cg@gmail.com>
2024-08-09 18:48:32 +0800

    url

--- src/World/World.ts
+++ src/World/World.ts
@@ -4,7 +4,7 @@ import { Camera, Scene, WebGLRenderer } from "three";
 export class World {
 
     constructor(container: HTMLElement | null, camera: Camera, scene: Scene, renderer: WebGLRenderer) {
-
+        // todo https://discoverthreejs.com/zh/book/first-steps/animation-loop/#the-update-loop
     }
 
     render() { }
