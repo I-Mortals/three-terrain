d5dadf33ebb049571ad35dfb5ea9a20c812b1b1d
steamdeck <wuchen.cg@gmail.com>
2024-08-09 18:46:49 +0800

    init

---
+++ src/World/World.ts
@@ -0,0 +1,11 @@
+import { Camera, Scene, WebGLRenderer } from "three";
+
+
+export class World {
+
+    constructor(container: HTMLElement | null, camera: Camera, scene: Scene, renderer: WebGLRenderer) {
+
+    }
+
+    render() { }
+}
---
+++ src/World/systems/Loop.ts
@@ -0,0 +1,40 @@
+import { Camera, Clock, Scene, WebGLRenderer } from "three";
+
+export class Loop {
+    camera: Camera;
+    scene: Scene;
+    renderer: WebGLRenderer;
+    updatables: any[] = [];
+    clock = new Clock();
+
+
+    constructor(camera: Camera, scene: Scene, renderer: WebGLRenderer) {
+        this.camera = camera;
+        this.scene = scene;
+        this.renderer = renderer;
+    }
+
+    start() {
+        this.renderer.setAnimationLoop(() => {
+            this.tick();
+            this.renderer.render(this.scene, this.camera)
+        })
+    }
+
+    stop() {
+        this.renderer.setAnimationLoop(null);
+    }
+    tick() {
+        // 每帧只调用一次getDelta函数
+        const delta = this.clock.getDelta();
+        console.log(
+            `The last frame rendered in ${delta * 1000} milliseconds`,
+        );
+
+        if (this.updatables.length < 1) return
+        for (const object of this.updatables) {
+            object.tick(delta);
+        }
+    }
+}
+
