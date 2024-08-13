0737ea6217a1f54d747ed0f5b35e2c1f0034ba55
steamdeck <wuchen.cg@gmail.com>
2024-08-13 18:37:53 +0800

    precision

--- src/World/components/renderer.ts
+++ src/World/components/renderer.ts
@@ -2,7 +2,10 @@ import { WebGLRenderer } from "three";
 
 
 export function createRenderer(width: number, height: number) {
-    const renderer = new WebGLRenderer({ antialias: true });
+    const renderer = new WebGLRenderer({ 
+        antialias: true, // 抗锯齿
+        precision: "highp", // highp 精度, mediump 精度,lowp 精度
+     });
     renderer.setSize(width, height);
     renderer.setPixelRatio(window.devicePixelRatio); // 像素比率
     return renderer
